e97463850bf22f0551ecfb736a710dfd4612700c
Airuxul <804754746@qq.com>
2025-07-26 20:12:34 +0800

    feature:优化下log

--- core/constants.py
+++ core/constants.py
@@ -7,16 +7,16 @@
 from enum import Enum
 
 # 工作流日志格式
-LOG_FLOW_START_FORMAT = "--- 开始执行工作流: {name} ---"
-LOG_FLOW_END_FORMAT = "--- 工作流: {name} 执行完毕 ---"
+LOG_FLOW_START_FORMAT = "[工作流开始]: {name}"
+LOG_FLOW_END_FORMAT = "[工作流结束]: {name}"
 
 class LogTreePreType(Enum):
     START = 'start'
     MID = 'mid'
     END = 'end'
 
-LOG_FLOW_START_TREE = "┣━"
-LOG_FLOW_MID_TREE = "┣━"
+LOG_FLOW_START_TREE = "┏━"
+LOG_FLOW_MID_TREE = "┣━" 
 LOG_FLOW_END_TREE = "┗━"
 
 LOG_TREE_INDENT = '  '  # 每层缩进2个空格
